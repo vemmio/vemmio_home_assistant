f28d8640caa410e46c31576221a1ea6c58e17aba
Mateusz Mamala <mateusz.mamala@gmail.com>
2025-12-04 10:35:20 +0100

    Added support for Vemmio Motion Detector device.

--- custom_components/vemmio/binary_sensor.py
+++ custom_components/vemmio/binary_sensor.py
@@ -14,7 +14,7 @@ from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 from . import VemmioConfigEntry
 from .const import LOGGER
 from .coordinator import VemmioDataUpdateCoordinator
-from .entity import VemmioEntity, async_setup_attribute_entities_binary_sensors
+from .entity import VemmioEntity, async_setup_attribute_entities_by_capability
 
 
 async def async_setup_entry(
@@ -26,16 +26,17 @@ async def async_setup_entry(
     coordinator = entry.runtime_data
     LOGGER.debug("Setting up Vemmio binary sensor for host %s", entry.data["host"])
 
-    async_setup_attribute_entities_binary_sensors(
-        hass, async_add_entities, coordinator, VemmioBinarySensor
+    async_setup_attribute_entities_by_capability(
+        hass, async_add_entities, coordinator, VemmioBinarySensor, "openClose"
+    )
+
+    async_setup_attribute_entities_by_capability(
+        hass, async_add_entities, coordinator, VemmioMotionSensor, "motionDetector"
     )
 
 
 class VemmioBinarySensor(VemmioEntity, BinarySensorEntity):
     """Defines a Vemmio binary sensor."""
-
-    _capability: Capability
-    _coordinator: VemmioDataUpdateCoordinator
 
     def __init__(
         self, coordinator: VemmioDataUpdateCoordinator, capability: Capability
@@ -43,13 +44,13 @@ class VemmioBinarySensor(VemmioEntity, BinarySensorEntity):
         """Initialize."""
 
         self._attr_device_class = BinarySensorDeviceClass.DOOR
-
         LOGGER.debug("Initializing Vemmio binary sensor")
         LOGGER.debug(str(coordinator.data))
         LOGGER.debug("Host: %s", coordinator.vemmio.host)
 
         super().__init__(coordinator=coordinator, capability=capability)
         self._attr_unique_id = f"binary_sensor_{capability.get_uuid_with_id()}"
+        self._capability = capability
         self._coordinator = coordinator
 
     @property
@@ -58,6 +59,40 @@ class VemmioBinarySensor(VemmioEntity, BinarySensorEntity):
         return self.coordinator.data.get_input_state(
             self._capability.node_uuid, self._capability.id
         )
+
+    async def async_update(self) -> None:
+        """Update entity."""
+        await self._coordinator.data.get_status()
+
+    @property
+    def should_poll(self) -> bool:
+        """Return True if entity has to be polled for state."""
+        return True
+
+
+class VemmioMotionSensor(VemmioEntity, BinarySensorEntity):
+    """Defines a Vemmio binary sensor."""
+
+    def __init__(
+        self, coordinator: VemmioDataUpdateCoordinator, capability: Capability
+    ) -> None:
+        """Initialize."""
+
+        self._attr_device_class = BinarySensorDeviceClass.MOTION
+
+        LOGGER.debug("Initializing Vemmio motion sensor")
+        LOGGER.debug(str(coordinator.data))
+        LOGGER.debug("Host: %s", coordinator.vemmio.host)
+
+        super().__init__(coordinator=coordinator, capability=capability)
+        self._attr_unique_id = f"motion_sensor_{capability.get_uuid_with_id()}"
+        self._capability = capability
+        self._coordinator = coordinator
+
+    @property
+    def is_on(self) -> bool:
+        """Return true if the motion sensor is on."""
+        return self.coordinator.data.get_motion_status_state()
 
     async def async_update(self) -> None:
         """Update entity."""
